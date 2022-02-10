02c0157ba5f0d64bfcd12b8436ddc39d5cddae9a
Mirjam Feigl <m.feigl@campus.lmu.de>
2022-02-10 09:07:41 +0100

    Add Landing Page

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import {Container, Row, Col} from 'react-bootstrap';
+import {Link} from "react-router-dom";
+
+function LandingPage(){
+    return <div>
+        <Container>
+            <Row>
+                <h1>Welcome to Meme Generator!</h1>
+            </Row>
+            </Container>
+            <Container>
+            <Row>
+                <Col><Button variant="primary" type="LogIn"><Link className="link" to="/SignUp">Sign Up</Link></Button></Col> 
+                <Col><Button variant="primary" type="LogIn"><Link className="link" to="/LogIn">Log In</Link></Button></Col>
+            </Row>
+        </Container>
+    </div>
+}
+
+export default LandingPage;
