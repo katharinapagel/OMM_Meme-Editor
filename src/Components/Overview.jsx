14a174311348bf7353f96f19f3c895d4e1ba5bf5
annaH <annahartm@gmail.com>
2022-02-11 15:14:05 +0100

    overview change

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -2,17 +2,11 @@ import React from "react";
 import { useLocation } from 'react-router-dom';
 
 function Overview(){
-    const location = useLocation();
-    const url = new URLSearchParams(location.search).get('url');
 
     return <div>
-        <h1> Great Job! Look at that meme!</h1>
-        
-        <div>
-        { url && <img alt='meme' src={url} /> }
-        </div>
-       
+        <h1>render all memes from the mongodb collection here </h1>
     </div>
+    
     
 }
 
