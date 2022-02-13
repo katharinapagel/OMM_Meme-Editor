db27b6563a4051be175b1ab974cb5b6440712234
Marinavdz <74961192+Marinavdz@users.noreply.github.com>
2022-02-13 10:56:13 +0100

    text-to-speech in Overview

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,59 +1,91 @@
 import axios from 'axios';
-import React, { useEffect, useState, Component } from "react";
-import {useSpeechSynthesis} from 'react-speech-kit';
+import React, { Component } from "react";
+
+const speechSynthesis = window.speechSynthesis;
+const voices = speechSynthesis.getVoices();
 
 //source: https://www.youtube.com/watch?v=Mfp94RjugWQ
 class Overview extends Component {
     constructor() {
         super();
         this.state = {
-            memes:[]
+            memes:[],
+            voiceIndex: -1,
+            voice: null
         };
     }
 
-componentDidMount = () =>{
-    this.getMemes();
-};
+    componentDidMount = () =>{
+        this.getMemes();
+    };
 
-//get memes from database
- getMemes =() =>{
-    axios.get("http://localhost:5000/api/meme/getMeme")
-    .then((response) => {
-        const data = response.data;
-        this.setState({ memes: data });
-        console.log("memes received");
-        console.log(data);
-    })
-    .catch(() => {
-        console.log("error");
-    });
-};
+    //get memes from database
+    getMemes =() =>{
+        axios.get("http://localhost:5000/api/meme/getMeme")
+        .then((response) => {
+            const data = response.data;
+            this.setState({ memes: data });
+            console.log("memes received");
+            console.log(data);
+        })
+        .catch(() => {
+            console.log("error");
+        });
+    };
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+    speak = (text) => {
+        var utterance = new SpeechSynthesisUtterance(text);
+        utterance.voice = voices[this.voiceIndex];
+        speechSynthesis.speak (utterance);
+    }
 
- displayMemes = (memes) =>{
-    if (!memes.length) return null;
+    displayMemes = (memes) =>{
+        if (!memes.length) return null;
 
-    return memes.map((memes, index) => (
-        <div key={index}>
-            <h3><b>{memes.title}</b>
-                 <button on Click={() => this.setState({})}><span role="img">🎤</span></button> 
-                 <label> <i>click me to play title</i> </label>
-            </h3> 
-            <h4>{memes.comments}</h4> 
-            <img src={memes.url} />
+        return memes.map((memes, index) => (
+            <div key={index}>
+                <h4><b>Title: {memes.title}</b>
+                    <button onClick = {()=> {this.speak ("The title of the meme is" + memes.title);}}><span role="img">🎤</span></button> 
+                    <label> <i>click me to play title</i> </label>
+                </h4> 
+                <h5>Comment: {memes.comments}</h5> 
+                <img src={memes.url} />
+                <p> </p>
+                <h1>---------------------------</h1>
+                <p> </p>
+            </div>
+            
+
+        ));
+
+    };
+
+    // source for label & select - https://github.com/MikeyParton/react-speech-kit/blob/master/examples/src/useSpeechSynthesis.jsx
+    render (){
+    return (
+        <div>
+            <label htmlFor="voice">Voice</label>
+                    <select
+                        id="voice"
+                        name="voice"
+                        value={this.voiceIndex}
+                        onChange={(event) => {
+                            this.voiceIndex = (event.target.value);
+                        }}
+                        >
+                        <option value="">Default</option>
+                        {voices.map((option, index) => (
+                            <option key={option.voiceURI} value={index}>
+                            {`${option.lang} - ${option.name}`}
+                            </option>
+                        ))}
+                    </select>
+            <div> <h1> <b>Look at all the memes!</b> </h1> </div>
+            <p></p>
+            <div> {this.displayMemes(this.state.memes)} </div>
         </div>
-
-    ));
-
-};
-
-render (){
-return (
-    <div>
-        <div> <h1> <b>Look at all the memes!</b> </h1> </div>
-        <div> {this.displayMemes(this.state.memes)} </div>
-    </div>
-    );
-}
+        );
+    }
 }
 export default Overview;
